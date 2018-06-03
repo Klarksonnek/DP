e9c777fa1a5acb83f2ad89d039d145d2fc7df92d
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-06-03 10:17:16 +0200

    uprava weka suboru
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- Peto/klasifikatory.sh
+++ Peto/klasifikatory.sh
@@ -11,29 +11,29 @@
 #	on the training data.
 
 
-WEKA="/home/panda/gits/diplomka/Peto/test.arff"
+WEKA_FILE="/home/panda/gits/diplomka/Peto/test.arff"
 
-cd /home/panda/Stažené/weka-3-8-2
+cd "/home/panda/Stiahnuté/weka-3-8-2"
 
 java -cp weka.jar weka.classifiers.functions.MultilayerPerceptron \
-     -t "/home/panda/gits/diplomka/Peto/test.arff" \
+     -t ${WEKA_FILE} \
      -L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++"
 
 java -cp weka.jar weka.classifiers.trees.J48 \
-     -t "/home/panda/gits/diplomka/Peto/test.arff" \
+     -t ${WEKA_FILE} \
      -C 0.25 -M 2
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++"
 
 java -cp weka.jar weka.classifiers.rules.ZeroR \
-     -t "/home/panda/gits/diplomka/Peto/test.arff"
+     -t ${WEKA_FILE} \
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++"
 
 java -cp weka.jar weka.classifiers.bayes.BayesNet \
-     -t "/home/panda/gits/diplomka/Peto/test.arff" \
+     -t ${WEKA_FILE} \
      -D -Q weka.classifiers.bayes.net.search.local.K2 -- -P 1 -S BAYES -E \
      weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 0.5 
 
@@ -45,27 +45,27 @@ SPLIT=66
 echo "+++++++++ split-percentage ${SPLIT} ++++++++++++"
 
 java -cp weka.jar weka.classifiers.functions.MultilayerPerceptron \
-     -t "/home/panda/gits/diplomka/Peto/test.arff" \
+     -t ${WEKA_FILE} \
      -split-percentage ${SPLIT} \
      -L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a 
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++"
 
 java -cp weka.jar weka.classifiers.trees.J48 \
-     -t "/home/panda/gits/diplomka/Peto/test.arff" \
+     -t ${WEKA_FILE} \
      -split-percentage ${SPLIT} \
      -C 0.25 -M 2 
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++"
 
 java -cp weka.jar weka.classifiers.rules.ZeroR \
-     -t "/home/panda/gits/diplomka/Peto/test.arff" \
+     -t ${WEKA_FILE} \
      -split-percentage ${SPLIT}
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++"
 
 java -cp weka.jar weka.classifiers.bayes.BayesNet \
-     -t "/home/panda/gits/diplomka/Peto/test.arff" \
+     -t ${WEKA_FILE} \
      -split-percentage ${SPLIT} \
      -D -Q weka.classifiers.bayes.net.search.local.K2 -- -P 1 -S BAYES -E \
      weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 0.5 
