ed0690225370694ee60c484bff4f58213c202f41
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-07-04 12:52:07 +0200

    BeeeOnClient: new class
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- env_dp/core.py
+++ env_dp/core.py
@@ -1,4 +1,6 @@
 import http.client
+import json
+import logging
 import ssl
 
 
@@ -43,5 +45,111 @@ class Rest:
 		return response, content
 
 
+class BeeeOnClient:
+	"""Client for communication with server supporting BeeeOn api."""
+
+	def __init__(self, host, port):
+		self.__host = host
+		self.__port = port
+		self.__api_key = ""
+		self.__token_id = ""
+		self.__log = logging.getLogger(self.__class__.__name__)
+
+	def refresh_token(self):
+		data = {'key': self.api_key, 'provider': 'apikey'}
+		try:
+			req = Rest(self.__host, self.__port, "/auth", "POST", False)
+			req.body(json.dumps(data))
+			res, body = req.perform()
+		except Exception as e:
+			self.__log.error(e)
+			return
+
+		json_res = json.loads(body)
+
+		if 'code' not in json_res:
+			raise LookupError('return code not found')
+
+		if int(json_res['code']) != 200:
+			self.__log.warning("invalid token_id")
+			exit(1)
+			return
+
+		return json_res['data']['id']
+
+	def sensors_info(self, gateway_id, device_id):
+		if not self.__token_id:
+			self.__token_id = self.refresh_token()
+
+		endpoint = '/gateways/' + str(gateway_id) + '/devices/' + str(device_id) + '/sensors'
+
+		req = Rest(self.__host, self.__port, endpoint, "GET", False)
+		req.authorize(self.__token_id)
+
+		res, body = req.perform()
+
+		return json.loads(body)['data']
+
+	def history(self, gateway, device, sensor, start, end, interval=1, aggregation='avg'):
+		if not self.__token_id:
+			self.__token_id = self.refresh_token()
+
+		endpoint = '/gateways/' + gateway
+		endpoint += '/devices/' + device
+		endpoint += '/sensors/' + str(sensor)
+		endpoint += '/history'
+		endpoint += '?range=' + str(start) + ',' + str(end)
+		endpoint += '&interval=' + str(interval)
+		endpoint += '&aggregation=' + aggregation
+
+		req = Rest(self.__host, self.__port, endpoint, "GET", False)
+		req.authorize(self.__token_id)
+
+		res, body = req.perform()
+
+		return json.loads(body)
+
+	def gateways(self):
+		if not self.__token_id:
+			self.__token_id = self.refresh_token()
+
+		endpoint = '/gateways'
+
+		req = Rest(self.__host, self.__port, endpoint, "GET", False)
+		req.authorize(self.__token_id)
+
+		res, body = req.perform()
+
+		return json.loads(body)['data']
+
+	def logout(self):
+		if not self.__token_id:
+			return
+
+		endpoint = '/auth'
+
+		req = Rest(self.__host, self.__port, endpoint, "DELETE", False)
+		req.authorize(self.__token_id)
+
+		req.perform()
+
+	@property
+	def rest_host(self):
+		return self.__host
+
+	@property
+	def rest_port(self):
+		return self.__port
+
+	@property
+	def api_key(self):
+		return self.__api_key
+
+	@api_key.setter
+	def api_key(self, key):
+		self.__api_key = key
+
+
 def main():
-	pass
+	beeeon_cl = BeeeOnClient("ant-work.fit.vutbr.cz", 8010)
+	beeeon_cl.api_key = "thaegeshecaz1EN9lutho0laeku1ahsh9eec5waeg0aiqua2buo7ieyoo0Shoow9ahpoosomie0weiqu"
