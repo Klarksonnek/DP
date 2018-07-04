a5616f3ee71fe781f8b20a7b4aea0cf8b75b7b05
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-07-04 12:47:45 +0200

    Rest: new class
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- env_dp/core.py
+++ env_dp/core.py
@@ -1,2 +1,47 @@
+import http.client
+import ssl
+
+
+class Rest:
+	"""Simple rest api client."""
+
+	def __init__(self, host, port, end_point, method, verify_ssl_cert=True):
+		self.__host = host
+		self.__port = port
+		self.__method = method
+		self.__end_point = end_point
+		self.__headers = {}
+		self.__body = None
+		self.__verify_ssl_cert = verify_ssl_cert
+
+	def __setitem__(self, key, value):
+		self.__headers[key] = value
+
+	def __getitem__(self, key):
+		return self.__headers[key]
+
+	def __delitem__(self, key):
+		del self.__headers[key]
+
+	def body(self, body):
+		self.__body = body
+
+	def authorize(self, session_id):
+		self.__headers['Authorization'] = 'Bearer ' + session_id
+
+	def perform(self):
+		if self.__verify_ssl_cert:
+			conn = http.client.HTTPSConnection(self.__host, self.__port, timeout=10)
+		else:
+			conn = http.client.HTTPSConnection(self.__host, self.__port, context=ssl._create_unverified_context(), timeout=10)
+
+		conn.request(self.__method, self.__end_point, headers=self.__headers, body=self.__body)
+
+		response = conn.getresponse()
+		content = str(response.read(), 'utf-8')
+		conn.close()
+		return response, content
+
+
 def main():
 	pass
