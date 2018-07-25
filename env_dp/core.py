7623da102fdfc284e54c9a016729fbb5e040098b
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-07-25 18:43:46 +0200

    DataStorage: new class with examples
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- env_dp/core.py
+++ env_dp/core.py
@@ -1,3 +1,4 @@
+import copy
 import datetime
 import hashlib
 import http.client
@@ -296,6 +297,352 @@ class WeatherData:
                 wind_speed = wind_speed + wspd_increase
 
         return out_detailed
+
+
+class DataStorage:
+    def __init__(self, client, weather_client, precision=1):
+        self.__client = client
+        self.__meta_data = []
+        self.__log = logging.getLogger(self.__class__.__name__)
+        self.__weather_client = weather_client
+        self.__precision = precision
+
+    def __parser_date(self, date):
+        return datetime.datetime.strptime(date, "%Y/%m/%d %H:%M:%S").timestamp()
+
+    def __download_sensor_modules(self, gateway, device):
+        data = self.__client.sensors_info(gateway, device)
+
+        out = []
+        for sensor in data:
+            out.append({
+                'id': int(sensor['id']),
+                'type_id': sensor['type_id']
+            })
+
+        return out
+
+    def __requested_modules(self, gateway, device, sensors):
+        supported_modules = self.__download_sensor_modules(gateway, device)
+
+        out = []
+        for sensor in sensors:
+            found = False
+            for supported_module in supported_modules:
+                if sensor['id'] == supported_module['id']:
+                    supported_module['custom_name'] = sensor['custom_name']
+                    out.append(supported_module)
+                    found = True
+                    break
+
+            if not found:
+                self.__log.warning("module %s not supported" % sensor)
+
+        return out
+
+    def read_meta_data(self, devices, events):
+        with open(devices) as f:
+            j_devices = json.load(f)
+
+        with open(events) as f:
+            j_events = json.load(f)
+
+        for event in j_events['events']:
+            new = copy.deepcopy(event)
+            new['times']['event_start'] = int(
+                self.__parser_date(event['times']['event_start']))
+            new['data'] = []
+            del (new['devices'])
+
+            types = ['event_start', 'no_event_start']
+
+            for type_id in types:
+                one_sensor = {
+                    'type': type_id,
+                    'weather': [],
+                    'values': [],
+                }
+
+                for key, device in event['devices'].items():
+                    for dev in j_devices['devices']:
+                        if dev['id'] != key:
+                            continue
+
+                        sensors = self.__requested_modules(dev['gateway'], dev['device'],
+                                                           dev['sensors'])
+
+                        for event_device in device:
+                            found = False
+                            for sensor in sensors:
+                                if event_device != sensor['custom_name']:
+                                    continue
+
+                                found = True
+                                one_sensor['values'].append({
+                                    'id': key,
+                                    'measured': [],
+                                    'module_id': sensor['id'],
+                                    'custom_name': sensor['custom_name'],
+                                    'type_id': sensor['type_id'],
+                                    'gateway': dev['gateway'],
+                                    'device': dev['device']
+                                })
+                                break
+
+                            if not found:
+                                self.__log.warning(
+                                    'sensor %s not found in input files with devices'
+                                    % event_device)
+
+                new['data'].append(one_sensor)
+            self.__meta_data.append(new)
+
+    def download_data(self, shift_before, shift_after):
+        out_json = copy.deepcopy(self.__meta_data)
+
+        for event in out_json:
+            for event_type in event['data']:
+                e_start_before_timestamp = int(
+                    float(event['times'][event_type['type']]) - shift_before)
+                e_start_after_timestamp = int(
+                    float(event['times'][event_type['type']]) + shift_after)
+
+                event_type['weather'] = self.__weather_client.weather_data(
+                    e_start_before_timestamp, e_start_after_timestamp)
+
+                for module in event_type['values']:
+                    if module['type_id'] == 'open_close':
+                        if 'no_event_start' in event_type['type']:
+                            module['measured'].append({
+                                'at': event['times']['no_event_start'],
+                                'value': '1.0'
+                            })
+                            continue
+
+                    module['measured'] = self.__client.history(
+                        module['gateway'],
+                        module['device'],
+                        module['module_id'],
+                        e_start_before_timestamp,
+                        e_start_after_timestamp
+                    )['data']
+
+        return out_json
+
+    def __switch_value(self, value):
+        if value == 1:
+            return 0
+
+        return 1
+
+    def __generate_event_data(self, event):
+        out = copy.deepcopy(event)
+        out['values'] = []
+
+        for item in event['values']:
+            if len(item['measured']) <= 1:
+                if item['type_id'] == 'open_close':
+                    out['values'].append(item)
+                    continue
+
+                if len(item['measured']) == 0:
+                    self.__log.debug(
+                        'prazdne hodnoty v modulu: %s, skip' % item['custom_name'])
+                    continue
+
+                if len(item['measured']) == 1 and item['type_id'] != 'open_close':
+                    self.__log.debug(
+                        'len jedna hodnota v modulu: %s, skip' % item['custom_name'])
+                    continue
+
+            out_values = []
+            for i in range(0, len(item['measured']) - 1):
+                value_start = item['measured'][i]['value']
+                value_end = item['measured'][i + 1]['value']
+
+                if value_start is None or value_end is None:
+                    continue
+
+                value_start = float(item['measured'][i]['value'])
+                value_end = float(item['measured'][i + 1]['value'])
+
+                time_start = item['measured'][i]['at']
+                time_end = item['measured'][i + 1]['at']
+
+                if value_start - value_end == 0:
+                    value_increase = 0
+                else:
+                    value_diff = value_end - value_start
+                    value_increase = value_diff / (time_end - time_start)
+
+                value = value_start
+                for j in range(0, time_end - time_start):
+                    out_values.append({
+                        "at": time_start + j,
+                        "value": round(value, self.__precision)
+                    })
+                    value = value + value_increase
+
+            out_values.append({
+                "at": item['measured'][len(item['measured']) - 1]['at'],
+                "value": round(value, self.__precision)
+            })
+
+            item['measured'] = out_values
+            out['values'].append(item)
+
+        return out
+
+    def __find_before_after_shift(self, events, event_type):
+        shift_before = None
+        shift_after = None
+
+        for event in events:
+            for data in event['data']:
+                if data['type'] != event_type:
+                    continue
+
+                time = event['times'][event_type]
+                for item in data['values']:
+                    values_cnt = len(item['measured']) - 1
+                    if len(item['measured']) <= 1 or item['type_id'] == "open_close":
+                        continue
+
+                    first = item['measured'][0]['at']
+                    last = item['measured'][values_cnt]['at']
+                    if shift_before is None:
+                        shift_before = time - first
+                    elif shift_before > time - first:
+                        shift_before = time - first
+
+                    if shift_after is None:
+                        shift_after = last - time
+                    elif last - time < shift_after:
+                        shift_after = last - time
+
+        return shift_before, shift_after
+
+    def __cut_common_data(self, event, data, times):
+        a, b = self.__find_before_after_shift(data, 'event_start')
+        c, d = self.__find_before_after_shift(data, 'no_event_start')
+
+        event = self.__generate_event_data(event)
+
+        local_min = None
+        local_max = None
+
+        out = []
+        for item in event['values']:
+            measured_out = []
+
+            if event['type'] == 'event_start':
+                local_min = times['event_start'] - a
+                local_max = times['event_start'] + b
+            elif event['type'] == 'no_event_start':
+                local_min = times['no_event_start'] - c
+                local_max = times['no_event_start'] + d
+
+            if item['type_id'] == "open_close":
+                value = 0
+                switch = True
+
+                if event['type'] == 'no_event_start':
+                    switch = False
+
+                for timestamp in range(local_min, local_max + 1):
+                    if timestamp >= item['measured'][0]['at'] and switch:
+                        value = self.__switch_value(value)
+                        switch = False
+
+                    measured_out.append({
+                        "at": timestamp,
+                        "value": round(float(value), self.__precision)
+                    })
+
+                item['measured'] = measured_out
+            else:
+                measured_out = []
+                for value in item['measured']:
+                    if value['at'] < local_min or value['at'] > local_max:
+                        continue
+
+                    measured_out.append(value)
+
+                item['measured'] = measured_out
+            out.append(item)
+
+        weather_out = []
+        for item in event['weather']:
+            if item['at'] < local_min or item['at'] > local_max:
+                continue
+
+            weather_out.append(item)
+
+        event['values'] = out
+        event['weather'] = weather_out
+
+        return event
+
+    def common_data(self, data):
+        out = []
+        for row in data:
+            out_row = copy.deepcopy(row)
+            out_row['data'] = []
+
+            for event_type in row['data']:
+                values = self.__cut_common_data(event_type, data, row['times'])
+                out_row['data'].append(values)
+
+            out.append(out_row)
+
+        return out
+
+    def __common_count(self, events, event_type):
+        count_before = None
+        count_after = None
+
+        for event in events:
+            for data in event['data']:
+                if data['type'] != event_type:
+                    continue
+
+                local_before = 0
+                local_after = 0
+                for value in data['values'][0]['measured']:
+                    if value['tag'] == 'before':
+                        local_before += 1
+                    elif value['tag'] == 'after':
+                        local_after += 1
+                    else:
+                        print('skip')
+
+                if count_before is None:
+                    count_before = local_before
+                else:
+                    count_before = min(count_before, local_before)
+
+                if count_after is None:
+                    count_after = local_after
+                else:
+                    count_after = min(count_after, local_after)
+
+        return count_before, count_after
+
+    def set_no_event_time(self, no_event_start_time_shift):
+        """
+        Vytvorenie casovej znacky, kde sa event nevyskytoval. Znacka sa vytvori pre zaciatok a
+        koniec eventu osobitne. Hodnoty pre zaciatok a koniec eventu sa vypocitaju posunom
+        o zadanu hodnotu dopredu (kladna hodnota) alebo dozadu (zaporna hodnota). Dopredu je
+        mysleny cas, ktory je vacsi ako aktualny.
+        """
+
+        for e in self.__meta_data:
+            new_value = e['times']['event_start'] + no_event_start_time_shift
+            e['times']['no_event_start'] = new_value
+
+    @property
+    def meta_data(self):
+        return self.__meta_data
 
 
 def api_key(filename='api_key.config'):
---
+++ examples/0004_data_storage_with_cache/run.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from os.path import dirname, abspath, join
+import sys
+
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '../..', ''))
+sys.path.append(CODE_DIR)
+
+import json
+import env_dp.core as dp
+import logging
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    client = dp.BeeeOnClient("ant-work.fit.vutbr.cz", 8010, cache=True)
+    client.api_key = dp.api_key(CODE_DIR + '/api_key.config')
+
+    storage = dp.DataStorage(client, dp.WeatherData(cache=True))
+    storage.read_meta_data('devices.json', 'events.json')
+    storage.set_no_event_time(-10)
+
+    dw = storage.download_data(10, 6)
+    client.logout()
+
+    common_data = storage.common_data(dw)
+    print(json.dumps(common_data, indent=4, sort_keys=True))
