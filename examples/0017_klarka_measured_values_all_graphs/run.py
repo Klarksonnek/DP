ecdcecc4cea3b868f0b8248c7ccdc29c250c67cf
Klara Necasova <xnecas24@stud.fit.vutbr.cz>
2018-10-08 13:12:15 +0200

    examples: edit example 0017
    Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

--- examples/0017_klarka_measured_values_all_graphs/run.py
+++ examples/0017_klarka_measured_values_all_graphs/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from os.path import dirname, abspath, join
+import datetime
 import sys
 
 THIS_DIR = dirname(__file__)
@@ -23,37 +24,53 @@ if __name__ == '__main__':
     modules = ['temperature_in', 'humidity_in', 'temperature_out', 'humidity_out']
     all = storage.download_data_for_normalization(modules)
 
+    all = dp.convert_relative_humidity_to_specific_humidity(all, 'temperature_in', 'humidity_in')
+    all = dp.convert_relative_humidity_to_specific_humidity(all, 'temperature_out', 'humidity_out')
+
     norm = dp.norm_all(all)
+    filtered = storage.filter_downloaded_data(norm, 'temperature_in', 'value',
+                                              'temperature_out', 'value', 5.0, 100.0)
+    filtered = storage.filter_downloaded_data_one_module(filtered, 'temperature_out', 'value', 15.0)
+    filtered = storage.filter_downloaded_data_two_conditions(filtered, 'humidity_out', 'specific_humidity', 6.0, 'humidity_in', 'specific_humidity', 1.6, 100.0)
+    filtered = storage.filter_downloaded_data_general_attribute(filtered, "window", "ventilacka")
+    filtered = dp.estimate_relative_humidity(filtered, 'humidity_in', 'humidity_out', 'temperature_in')
+    print("Event count: %d" % len(filtered))
 
     one_norm_graph = []
     graphs = []
 
-    for item in norm:
-        norm_values_temp_in = dp.filter_one_values(item, 'temperature_in')
-        norm_values_hum_in = dp.filter_one_values(item, 'humidity_in')
-        norm_values_temp_out = dp.filter_one_values(item, 'temperature_out')
-        norm_values_hum_out = dp.filter_one_values(item, 'humidity_out')
+    for i in range(0, len(filtered)):
+        norm_values_temp_in = dp.filter_one_values(filtered[i], 'temperature_in')
+        norm_values_hum_in = dp.filter_one_values(filtered[i], 'humidity_in')
+        norm_values_temp_out = dp.filter_one_values(filtered[i], 'temperature_out')
+        norm_values_hum_out = dp.filter_one_values(filtered[i], 'humidity_out')
+        start = filtered[i]['times']['event_start']
+        end = filtered[i]['times']['event_end']
+
+        t = datetime.datetime.fromtimestamp(start).strftime('%d.%m. %H:%M:%S')
+        t += ' - '
+        t += datetime.datetime.fromtimestamp(end).strftime('%H:%M:%S')
+
+        #g = {
+        #    'title': 'Temp in and hum in',
+        #    'graphs': [
+        #        dp.gen_simple_graph(norm_values_temp_in, 'red', 'temp in', 'value_norm', 100),
+        #        dp.gen_simple_graph(norm_values_hum_in, 'blue', 'hum in', 'value_norm', 100)
+        #    ]
+        #}
+        #graphs.append(g)
+
+        #g = {
+        #    'title': 'Temp out and hum out',
+        #    'graphs': [
+        #        dp.gen_simple_graph(norm_values_temp_out, 'red', 'temp out', 'value_norm', 100),
+        #        dp.gen_simple_graph(norm_values_hum_out, 'blue', 'hum out', 'value_norm', 100)
+        #    ]
+        #}
+        #graphs.append(g)
 
         g = {
-            'title': 'Temp in and hum in',
-            'graphs': [
-                dp.gen_simple_graph(norm_values_temp_in, 'red', 'temp in', 'value_norm', 100),
-                dp.gen_simple_graph(norm_values_hum_in, 'blue', 'hum in', 'value_norm', 100)
-            ]
-        }
-        graphs.append(g)
-
-        g = {
-            'title': 'Temp out and hum out',
-            'graphs': [
-                dp.gen_simple_graph(norm_values_temp_out, 'red', 'temp out', 'value_norm', 100),
-                dp.gen_simple_graph(norm_values_hum_out, 'blue', 'hum out', 'value_norm', 100)
-            ]
-        }
-        graphs.append(g)
-
-        g = {
-            'title': 'Temp in and temp out',
+            'title': 'Temp in and temp out ' + t,
             'graphs': [
                 dp.gen_simple_graph(norm_values_temp_in, 'DarkRed', 'temp in', 'value', 100),
                 dp.gen_simple_graph(norm_values_temp_out, 'LightCoral', 'temp out', 'value', 100)
@@ -63,14 +80,45 @@ if __name__ == '__main__':
         graphs.append(g)
 
         g = {
-            'title': 'Hum in and hum out',
+            'title': 'Hum in and hum out ' + t,
+            'graphs': [
+                dp.gen_simple_graph(norm_values_hum_in, 'blue', 'hum in', 'value', 100),
+                dp.gen_simple_graph(norm_values_hum_out, 'red', 'hum out', 'value', 100)
+            ]
+        }
+
+        graphs.append(g)
+
+        g = {
+            'title': 'Spec hum in and spec hum out ' + t,
+            'graphs': [
+                dp.gen_simple_graph(norm_values_hum_in, 'blue', 'hum in', 'specific_humidity', 100),
+                dp.gen_simple_graph(norm_values_hum_out, 'red', 'hum out', 'specific_humidity', 100)
+            ]
+        }
+
+        graphs.append(g)
+
+        g = {
+            'title': 'Hum in and hum in estimated ' + t,
+            'graphs': [
+                dp.gen_simple_graph(norm_values_hum_in, 'blue', 'hum in', 'value', 100),
+                dp.gen_simple_graph(norm_values_hum_out, 'red', 'hum out', 'value', 100)
+            ]
+        }
+
+        graphs.append(g)
+
+        g = {
+            'title': 'Hum in and hum in estimated ' + t,
             'graphs': [
                 dp.gen_simple_graph(norm_values_hum_in, 'DarkBlue', 'hum in', 'value', 100),
-                dp.gen_simple_graph(norm_values_hum_out, 'DarkTurquoise', 'hum out', 'value', 100)
+                dp.gen_simple_graph(norm_values_hum_in, 'DarkRed', 'hum in estimated', 'hum_in_estimated', 100)
             ]
         }
 
         graphs.append(g)
 
     g = dp.Graph("./../../src/graph")
+    # g = dp.Graph("src/graph")
     g.gen(graphs, 'test_g.html', 0, 0)
