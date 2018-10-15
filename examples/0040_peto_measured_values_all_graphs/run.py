dd4fac1a9c8d28558a098c16c0efdf4270b0c451
Klara Necasova <xnecas24@stud.fit.vutbr.cz>
2018-10-15 13:29:54 +0200

    examples: add statistics to graphs
    Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

--- examples/0040_peto_measured_values_all_graphs/run.py
+++ examples/0040_peto_measured_values_all_graphs/run.py
@@ -26,6 +26,8 @@ if __name__ == '__main__':
     all = storage.filter_general_attribute_value(all, 'out_sensor', 'yes')
     all = dp.convert_relative_humidity_to_specific_humidity(all, 'beeeon_temperature_in', 'beeeon_humidity_in')
     all = dp.convert_relative_humidity_to_specific_humidity(all, 'beeeon_temperature_out', 'beeeon_humidity_out')
+    all = dp.convert_relative_humidity_to_absolute_humidity(all, 'beeeon_temperature_in', 'beeeon_humidity_in')
+    all = dp.convert_relative_humidity_to_absolute_humidity(all, 'beeeon_temperature_out', 'beeeon_humidity_out')
 
     norm = dp.norm_all(all)
 
@@ -45,6 +47,39 @@ if __name__ == '__main__':
         norm_values_hum_in = dp.filter_one_values(filtered[i], 'beeeon_humidity_in')
         norm_values_temp_out = dp.filter_one_values(filtered[i], 'beeeon_temperature_out')
         norm_values_hum_out = dp.filter_one_values(filtered[i], 'beeeon_humidity_out')
+
+        precision = 2
+
+        stat = [
+            ('people', filtered[i]['people']),
+            ('wind', filtered[i]['wind']),
+            ('obloha', filtered[i]['sky']),
+            ('slnko', filtered[i]['sun']),
+            ('', ''),
+            ('teplota dnu', round(norm_values_temp_in[0]['value'], precision)),
+            ('teplota von', round(norm_values_temp_out[0]['value'], precision)),
+            ('rozdiel teplot',
+             round(abs(norm_values_temp_out[0]['value'] - norm_values_temp_in[0]['value']), precision)),
+
+            ('', ''),
+            ('rh dnu', round(norm_values_hum_in[0]['value'], precision)),
+            ('rh von', round(norm_values_hum_out[0]['value'], precision)),
+            ('rozdiel rh', round(abs(norm_values_hum_in[0]['value'] - norm_values_hum_out[0]['value']), precision)),
+
+            ('', ''),
+            ('abs rh dnu', round(norm_values_hum_in[0]['absolute_humidity'], precision)),
+            ('abs rh von', round(norm_values_hum_out[0]['absolute_humidity'], precision)),
+            ('rozdiel abs rh',
+             round(abs(norm_values_hum_in[0]['absolute_humidity'] - norm_values_hum_out[0]['absolute_humidity']),
+                   precision)),
+
+            ('', ''),
+            ('spec rh dnu', round(norm_values_hum_in[0]['specific_humidity'], precision)),
+            ('spec rh von', round(norm_values_hum_out[0]['specific_humidity'], precision)),
+            ('rozdiel spec rh',
+             round(abs(norm_values_hum_in[0]['specific_humidity'] - norm_values_hum_out[0]['specific_humidity']),
+                   precision)),
+        ]
 
         #g = {
         #    'title': 'Temp in and hum in',
@@ -66,6 +101,7 @@ if __name__ == '__main__':
 
         g = {
             'title': 'Temp in and temp out',
+            'stat': stat,
             'graphs': [
                 dp.gen_simple_graph(norm_values_temp_in, 'DarkRed', 'temp in', 'value', 100),
                 dp.gen_simple_graph(norm_values_temp_out, 'LightCoral', 'temp out', 'value', 100)
