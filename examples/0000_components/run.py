74a0f7e27e94c943e770f845f503ddb4f6e4be73
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-10-22 20:06:38 +0200

    components: move example with download weather with cache
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- examples/0000_components/run.py
+++ examples/0000_components/run.py
@@ -20,7 +20,17 @@ def download_weather_without_cache():
     logging.info(json.dumps(w.weather_data(1525503999, 1525504015), indent=4, sort_keys=True))
 
 
+def download_weather_with_cache():
+    w = dp.WeatherData(cache=True)
+
+    logging.info(json.dumps(w.weather_data(1525240913, 1525240929), indent=4, sort_keys=True))
+    logging.info(json.dumps(w.weather_data(1525240903, 1525240919), indent=4, sort_keys=True))
+    logging.info(json.dumps(w.weather_data(1525504009, 1525504025), indent=4, sort_keys=True))
+    logging.info(json.dumps(w.weather_data(1525503999, 1525504015), indent=4, sort_keys=True))
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     download_weather_without_cache()
+    download_weather_with_cache()
