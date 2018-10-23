a9ac23c6d73f88d8731eea08eba482e43b221b39
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-10-23 20:25:06 +0200

    example: move example from 0106 to function in example 0105
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- examples/0105_klarka_filtered_temp_histogram_from_all_graphs/run.py
+++ examples/0105_klarka_filtered_temp_histogram_from_all_graphs/run.py
@@ -26,6 +26,20 @@ def filtered_temp_histogram_from_all_graphs(events):
     g.gen(histograms, 'histogram_1.html', 0, 0, 'bar')
 
 
+def filtered_hum_histogram_from_all_graphs(events):
+    filtered = storage.filter_downloaded_data(events, 'temperature_in', 'value',
+                                              'temperature_out', 'value', 7.0, 100.0)
+    filtered = storage.filter_downloaded_data(filtered, 'humidity_in', 'value',
+                                              'humidity_out', 'value', 15.0, 100.0)
+
+    filtered_one_value = dp.filter_data(filtered, ['temperature_in'])
+    his_data = dp.gen_histogram(filtered_one_value, 30, 20, 70, 5, 'value')
+    histograms = dp.gen_histogram_graph(his_data)
+
+    g = dp.Graph("./../../src/graph")
+    g.gen(histograms, 'histogram_2.html', 0, 0, 'bar')
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
@@ -41,3 +55,5 @@ if __name__ == '__main__':
     norm = dp.norm_all(all)
 
     filtered_temp_histogram_from_all_graphs(copy.deepcopy(norm))
+    filtered_hum_histogram_from_all_graphs(copy.deepcopy(norm))
+
