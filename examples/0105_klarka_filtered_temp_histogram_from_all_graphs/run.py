bdaaa5d4e48864648b9cfb3eff30c9feff2050f5
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-10-23 20:33:28 +0200

    example: move example from 0102 to function in example 0105
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- examples/0105_klarka_filtered_temp_histogram_from_all_graphs/run.py
+++ examples/0105_klarka_filtered_temp_histogram_from_all_graphs/run.py
@@ -40,6 +40,15 @@ def filtered_hum_histogram_from_all_graphs(events):
     g.gen(histograms, 'histogram_2.html', 0, 0, 'bar')
 
 
+def temp_histogram_from_all_graphs(events):
+    filtered_one_value = dp.filter_data(events, ['temperature_in'])
+    his_data = dp.gen_histogram(filtered_one_value, 20, 0, 1, 0.1, 'value_norm')
+    histograms = dp.gen_histogram_graph(his_data)
+
+    g = dp.Graph("./../../src/graph")
+    g.gen(histograms, 'histogram_3.html', 0, 0, 'bar')
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
@@ -56,4 +65,4 @@ if __name__ == '__main__':
 
     filtered_temp_histogram_from_all_graphs(copy.deepcopy(norm))
     filtered_hum_histogram_from_all_graphs(copy.deepcopy(norm))
-
+    temp_histogram_from_all_graphs(copy.deepcopy(norm))
