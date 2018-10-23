f79cc9d3a6a16d7d6abb511eef0ec206069d0fad
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-10-23 20:23:52 +0200

    example: move example from 0105 to function in example 0105
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- examples/0105_klarka_filtered_temp_histogram_from_all_graphs/run.py
+++ examples/0105_klarka_filtered_temp_histogram_from_all_graphs/run.py
@@ -9,6 +9,21 @@ sys.path.append(CODE_DIR)
 
 import env_dp.core as dp
 import logging
+import copy
+
+
+def filtered_temp_histogram_from_all_graphs(events):
+    filtered = storage.filter_downloaded_data(events, 'temperature_in', 'value',
+                                              'temperature_out', 'value', 7.0, 100.0)
+    filtered = storage.filter_downloaded_data(filtered, 'humidity_in', 'value',
+                                              'humidity_out', 'value', 15.0, 100.0)
+
+    filtered_one_value = dp.filter_data(filtered, ['temperature_in'])
+    his_data = dp.gen_histogram(filtered_one_value, 10, 13, 31, 1, 'value')
+    histograms = dp.gen_histogram_graph(his_data)
+
+    g = dp.Graph("./../../src/graph")
+    g.gen(histograms, 'histogram_1.html', 0, 0, 'bar')
 
 
 if __name__ == '__main__':
@@ -24,14 +39,5 @@ if __name__ == '__main__':
     all = storage.download_data_for_normalization(modules)
 
     norm = dp.norm_all(all)
-    filtered = storage.filter_downloaded_data(norm, 'temperature_in', 'value',
-                                              'temperature_out', 'value', 7.0, 100.0)
-    filtered = storage.filter_downloaded_data(filtered, 'humidity_in', 'value',
-                                              'humidity_out', 'value', 15.0, 100.0)
 
-    filtered_one_value = dp.filter_data(filtered, ['temperature_in'])
-    his_data = dp.gen_histogram(filtered_one_value, 10, 13, 31, 1, 'value')
-    histograms = dp.gen_histogram_graph(his_data)
-
-    g = dp.Graph("./../../src/graph")
-    g.gen(histograms, 'test_g.html', 0, 0, 'bar')
+    filtered_temp_histogram_from_all_graphs(copy.deepcopy(norm))
