0ccb87040d88291c6cc2baddcb33acc125cc578d
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-11-27 21:18:43 +0100

    db_core: test db connection and travis support
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ env_dp/db_core.py
@@ -0,0 +1,25 @@
+import configparser
+import mysql.connector
+import os
+
+
+def create_con(config_file='/etc/dp/config.ini'):
+    # example travis hostname: travis-job-d072bd30-f722-4d10-*
+    hostname = os.uname()[1]
+    if 'travis' in hostname:
+        return mysql.connector.connect(
+            host='localhost',
+            user='root',
+            passwd='',
+            database='demo'
+        )
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    return mysql.connector.connect(
+        host=config['db']['host'],
+        user=config['db']['user'],
+        passwd=config['db']['passwd'],
+        database=config['db']['database']
+    )
