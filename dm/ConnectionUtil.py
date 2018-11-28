7e5153d170936f2f62082df24a844938bf1d6ec8
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-11-28 18:23:12 +0100

    dm.ConnectionUtil: move create con. to class
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ dm/ConnectionUtil.py
@@ -0,0 +1,34 @@
+import configparser
+import os
+import mysql.connector
+
+
+class ConnectionUtil:
+    @staticmethod
+    def create_con(config_file='/etc/dp/config.ini'):
+        # example travis hostname: travis-job-d072bd30-f722-4d10-*
+        hostname = os.uname()[1]
+        if 'travis' in hostname:
+            return mysql.connector.connect(
+                host='localhost',
+                user='root',
+                passwd='',
+                database='demo'
+            )
+
+        config = configparser.ConfigParser()
+        config.read(config_file)
+
+        return mysql.connector.connect(
+            host=config['db']['host'],
+            user=config['db']['user'],
+            passwd=config['db']['passwd'],
+            database=config['db']['database']
+        )
+
+    @staticmethod
+    def api_key(config_file='/etc/dp/config.ini'):
+        config = configparser.ConfigParser()
+        config.read(config_file)
+
+        return config['beeeon']['api.key']
