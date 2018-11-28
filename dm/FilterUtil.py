f0d5fd93f6606cd6b63136196bd58f628b1f837f
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-11-28 17:56:30 +0100

    dm.FilterUtil: new class
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ dm/FilterUtil.py
@@ -0,0 +1,71 @@
+class FilterUtil:
+    @staticmethod
+    def only_valid_events(events):
+        out = []
+
+        for event in events:
+            if event['valid_event']:
+                out.append(event)
+        return out
+
+    @staticmethod
+    def temperature_diff(events, min_value, max_value):
+        out = []
+
+        for event in events:
+            temp_in = event['measured']['temperature_in_celsius'][0]
+            temp_out = event['measured']['temperature_out_celsius'][0]
+
+            if min_value <= abs(temp_in - temp_out) <= max_value:
+                out.append(event)
+
+        return out
+
+    @staticmethod
+    def temperature_out_max(events, max_value):
+        out = []
+
+        for event in events:
+            temp_out = event['measured']['temperature_out_celsius'][0]
+
+            if temp_out < max_value:
+                out.append(event)
+
+        return out
+
+    @staticmethod
+    def humidity(events, min_out_specific_humidity, min_diff, max_diff):
+        out = []
+
+        for event in events:
+            specific_in = event['measured']['rh_in_specific_g_kg'][0]
+            specific_out = event['measured']['rh_out_specific_g_kg'][0]
+
+            if specific_out < min_out_specific_humidity:
+                out.append(event)
+                continue
+
+            if min_diff <= abs(specific_out - specific_in) <= max_diff:
+                out.append(event)
+
+        return out
+
+    @staticmethod
+    def attribute(events, attribute_name, value):
+        out = []
+
+        for event in events:
+            if event[attribute_name] == value:
+                out.append(event)
+
+        return out
+
+    @staticmethod
+    def attribute_exclude(events, attribute_name, value):
+        out = []
+
+        for event in events:
+            if event[attribute_name] != value:
+                out.append(event)
+
+        return out
