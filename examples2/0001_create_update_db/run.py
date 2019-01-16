243f4266d8e1f20e3281d7198efe9bf5493c9609
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-01-16 17:01:51 +0100

    example2: new script for create/update database
    Bol pripraveny novy script pre vytvorenie a aktualizaciu databaze.
    Po novom sa vytvori pre kazde merane miesto nova tabulka. Tabulky
    su rozdele na dva typy. Prvym typom je tabulka, ktora obsahuje
    vsetky hodnoty. Druha tabulka obsahuje len 4 hodnoty za minutu.
    Rozdelenie tabuliek je podla jej ucelov, niekedy treba tabulku
    vizualizovat a v tom pripade, je lepsie, ked tam nie je tolko dat.
    
    V novom scripte bola opravena chyba, v pripade, ze ak sa prerusila
    cinnost scripta a bol opatovne spusteny tak doslo k najdeniu poslednej
    zaznamenanej hodnoty len v jednej tabulke a v ostatnych tabulkach
    sa to nekontrolovova. Dochadzalo tak k nekonzistencii poctu zaznamov
    v tabulkach.
    
    Samotne generovanie bolo zjednodusene a pridala sa podpora pre senzory,
    ktore mozu byt zaznamenavane na rozne servery.
    
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- examples2/0001_create_update_db/run.py
+++ examples2/0001_create_update_db/run.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import sys
+import time
 from os.path import dirname, abspath, join
 
 CODE_DIR = abspath(join(dirname(__file__), '../..', ''))
@@ -20,20 +21,37 @@ def devices(filename='devices.json'):
     return data
 
 
-def create_table_fast_view(client, con, start, end, owner, devices, table_name, write_each):
-    last_open_close_state = 0
+def create_update_table(con, clients, start, end, devices, write_each, table_name):
     step_size = 600
     time_shift = 1200
-    min_commit_size = 100000
+    min_commit_size = 5000
+    last_inserted_row = DBUtil.last_inserted_values(con, table_name)
+
+    last_open_close_state = 0
     actual_commit_size = 0
 
-    for t in range(start, end, step_size):
-        logging.debug('processing interval: %s - %s' % (DateTimeUtil.utc_timestamp_to_str(t),
-                                                        DateTimeUtil.utc_timestamp_to_str(
-                                                        t + step_size, '%H:%M:%S')))
+    logging.info('table: %s' % table_name)
 
-        PreProcessing.prepare(client, con, table_name, devices, t, t + step_size,
-                              last_open_close_state, owner, time_shift, write_each=write_each)
+    for interval_from in range(start, end, step_size):
+        interval_to = interval_from + step_size
+
+        # ak sa v databaze nachadzaju nejake data a timestamp posledne vlozeneho casu,
+        # je vacsi ako aktualne spracovavany koniec intervalu, tak sa spracovanie
+        # tohto intervalu preskoci, inak sa zacne od tohto timestampu a tabulka sa doplna
+        # o nove udaje
+        if last_inserted_row is not None:
+            if interval_to < last_inserted_row[0]:
+                interval = DateTimeUtil.create_interval_str(interval_from,
+                                                            interval_from + step_size)
+
+                logging.debug('processed interval %s has been already inserted' % interval)
+                continue
+
+        logging.debug('processed interval %s' % DateTimeUtil.create_interval_str(interval_from,
+                                                                                 interval_to))
+
+        PreProcessing.prepare(clients, con, table_name, devices, interval_from, interval_to,
+                              last_open_close_state, time_shift, write_each=write_each)
 
         actual_commit_size += step_size // write_each
         if actual_commit_size > min_commit_size:
@@ -41,10 +59,12 @@ def create_table_fast_view(client, con, start, end, owner, devices, table_name, 
             con.commit()
             actual_commit_size = 0
 
-        last_open_close_state = DBUtil.last_inserted_open_close_state(con, table_name, owner)
+        last_open_close_state = DBUtil.last_inserted_open_close_state(con, table_name)
 
     logging.debug('commit %s rows' % actual_commit_size)
     con.commit()
+
+    logging.info('table %s created and updated' % table_name)
 
 
 if __name__ == '__main__':
@@ -53,73 +73,33 @@ if __name__ == '__main__':
     con = ConnectionUtil.create_con()
     cur = con.cursor()
 
-    cl = BeeeOnClient("ant-work.fit.vutbr.cz", 8010)
-    cl.api_key = ConnectionUtil.api_key()
+    beeonClient = BeeeOnClient("ant-work.fit.vutbr.cz", 8010)
+    cls = {
+        "ant-work": beeonClient,
+    }
+
+    cls['ant-work'].api_key = ConnectionUtil.api_key('ant-work')
 
     # from 2018/09/20 00:01:00
     start = int(DateTimeUtil.local_time_str_to_utc('2018/09/20 00:01:00').timestamp())
-    end = int(DateTimeUtil.local_time_str_to_utc('2018/11/24 22:59:59').timestamp())
+    end = int(time.time())
 
-    table1 = 'fast_view'
-    table2 = 'view_all'
-    DBUtil.create_table(con, table1)
-    DBUtil.create_table(con, table2)
+    # full db
+    table_pt = 'measured_peto'
+    DBUtil.create_table(con, table_pt)
+    # DBUtil.check_timestamp_order(con, table_pt)
+    create_update_table(con, cls, start, end, devices()['peto'], 1, table_pt)
 
-    generate_time = 60 * 60 * 24  # one day
-    last_generated_time = None
-    for time in range(start, end, generate_time):
-        if last_generated_time is None:
-            last_generated_time = time
-            continue
+    table_kl = 'measured_klarka'
+    DBUtil.create_table(con, table_kl)
+    # DBUtil.check_timestamp_order(con, table_kl)
+    create_update_table(con, cls, start, end, devices()['klarka'], 1, table_kl)
 
-        logging.debug('processing interval: %s - %s' %
-                      (DateTimeUtil.utc_timestamp_to_str(last_generated_time),
-                       DateTimeUtil.utc_timestamp_to_str(time)))
+    # faster db
+    table_pt = 'measured_peto_reducted'
+    DBUtil.create_table(con, table_pt)
+    create_update_table(con, cls, start, end, devices()['peto'], 15, table_pt)
 
-        # create or update fast_view table for Klarka
-        end1 = DBUtil.last_inserted_values(con, table1, 'Klarka')
-        # ak je end1 None, ziaden zaznam este nebol pridany do danej tabulky s danym ownerom
-        if end1 is None or end1[0] < time:
-            if end1 is None:
-                s = last_generated_time
-            else:
-                s = end1[0]
-
-            logging.debug('finding of last inserted interval for table %s - Klarka', table1)
-            create_table_fast_view(cl, con, s, time, 'Klarka', devices()['klarka'], table1, 10)
-
-        # create or update fast_view table for Peto
-        end1 = DBUtil.last_inserted_values(con, table1, 'Peto')
-        if end1 is None or end1[0] < time:
-            if end1 is None:
-                s = last_generated_time
-            else:
-                s = end1[0]
-
-            logging.debug('finding of last inserted interval for table %s - Peto',
-                          table1)
-            create_table_fast_view(cl, con, s, time, 'Peto', devices()['peto'], table1, 10)
-
-        # create or update view_all table for Klarka
-        end1 = DBUtil.last_inserted_values(con, table2, 'Klarka')
-        if end1 is None or end1[0] < time:
-            if end1 is None:
-                s = last_generated_time
-            else:
-                s = end1[0]
-
-            logging.debug('finding of last inserted interval for table %s - Klarka', table2)
-            create_table_fast_view(cl, con, s, time, 'Klarka', devices()['klarka'], table2, 1)
-
-        # create or update view_all table for Peto
-        end1 = DBUtil.last_inserted_values(con, table2, 'Peto')
-        if end1 is None or end1[0] < time:
-            if end1 is None:
-                s = last_generated_time
-            else:
-                s = end1[0]
-
-            logging.debug('finding of last inserted interval for table %s - Peto', table2)
-            create_table_fast_view(cl, con, s, time, 'Peto', devices()['peto'], table2, 1)
-
-        last_generated_time = time
+    table_kl = 'measured_klarka_reducted'
+    DBUtil.create_table(con, table_kl)
+    create_update_table(con, cls, start, end, devices()['klarka'], 15, table_kl)
