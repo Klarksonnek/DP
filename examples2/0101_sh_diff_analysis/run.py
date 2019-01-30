6dcfe741ce3238588004e4f9d4f5b1bde4289185
Klara Necasova <xnecas24@stud.fit.vutbr.cz>
2019-01-30 12:20:21 +0100

    test0101: fix value reading and add ah and temp diff
    Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

--- examples2/0101_sh_diff_analysis/run.py
+++ examples2/0101_sh_diff_analysis/run.py
@@ -111,21 +111,30 @@ def humidity_info_csv(events, start_shift, end_shift, precision=2):
         absolute_in_start = event['measured']['linear1_ah'][0 - (start_shift + 1)]
         absolute_in_end = event['measured']['linear1_ah'][-end_shift + 1]
 
+        temp_in_start = event['measured']['linear1_temp'][0 - (start_shift + 1)]
+        temp_in_end = event['measured']['linear1_temp'][-end_shift + 1]
+
         specific_in_start_2 = event['measured']['linear2_sh'][0 - (start_shift + 1)]
         specific_in_end_2 = event['measured']['linear2_sh'][-end_shift + 1]
 
         absolute_in_start_2 = event['measured']['linear2_ah'][0 - (start_shift + 1)]
         absolute_in_end_2 = event['measured']['linear2_ah'][-end_shift + 1]
 
-        absolute_in = event['measured']['rh_in_absolute_g_m3'][0]
-        absolute_out = event['measured']['rh_out_absolute_g_m3'][0]
+        temp_in_start_2 = event['measured']['linear2_temp'][0 - (start_shift + 1)]
+        temp_in_end_2 = event['measured']['linear2_temp'][-end_shift + 1]
 
-        specific_in = event['measured']['rh_in_specific_g_kg'][0]
-        specific_out = event['measured']['rh_out_specific_g_kg'][0]
+        specific_in = event['measured']['rh_in_specific_g_kg'][0 - (start_shift + 1)]
+        specific_out = event['measured']['rh_out_specific_g_kg'][-end_shift + 1]
 
-        absolute_in_2 = event['measured']['rh_in2_absolute_g_m3'][0]
+        absolute_in = event['measured']['rh_in_absolute_g_m3'][0 - (start_shift + 1)]
+        absolute_out = event['measured']['rh_out_absolute_g_m3'][-end_shift + 1]
 
-        specific_in_2 = event['measured']['rh_in2_specific_g_kg'][0]
+        temp_in = event['measured']['temperature_in_celsius'][0 - (start_shift + 1)]
+        temp_out = event['measured']['temperature_out_celsius'][-end_shift + 1]
+
+        specific_in_2 = event['measured']['rh_in2_specific_g_kg'][0 - (start_shift + 1)]
+        absolute_in_2 = event['measured']['rh_in2_absolute_g_m3'][0 - (start_shift + 1)]
+        temp_in_2 = event['measured']['temperature_in2_celsius'][0 - (start_shift + 1)]
 
         out.append(OrderedDict([
             ('start_datetime', event['e_start']['readable']),
@@ -134,10 +143,14 @@ def humidity_info_csv(events, start_shift, end_shift, precision=2):
             ('diff_sh_1', round(abs(specific_out - specific_in), precision)),
             ('diff_ah_in_1', round(abs(absolute_in_start - absolute_in_end), precision)),
             ('diff_ah_1', round(abs(absolute_out - absolute_in), precision)),
+            ('diff_temp_in_1', round(abs(temp_in_start - temp_in_end), precision)),
+            ('diff_temp_1', round(abs(temp_out - temp_in), precision)),
             ('diff_sh_in_2', round(abs(specific_in_start_2 - specific_in_end_2), precision)),
             ('diff_sh_2', round(abs(specific_out - specific_in_2), precision)),
             ('diff_ah_in_2', round(abs(absolute_in_start_2 - absolute_in_end_2), precision)),
-            ('diff_ah_2', round(abs(absolute_out - absolute_in_2), precision))
+            ('diff_ah_2', round(abs(absolute_out - absolute_in_2), precision)),
+            ('diff_temp_in_2', round(abs(temp_in_start_2 - temp_in_end_2), precision)),
+            ('diff_temp_2', round(abs(temp_out - temp_in_2), precision))
         ]))
 
     CSVUtil.create_csv_file(out, 'humidity_info.csv')
@@ -169,12 +182,14 @@ def main(events_file: str, start_shift: int, end_shift: int, output_filename: st
     logging.info('event count: %d for senzor 1' % len(sensor1_events))
     linear_reg(sensor1_events, 'rh_in_specific_g_kg', 'linear1_sh')
     linear_reg(sensor1_events, 'rh_in_absolute_g_m3', 'linear1_ah')
+    linear_reg(sensor1_events, 'temperature_in_celsius', 'linear1_temp')
 
     # generovanie grafov pre senzor 1
     logging.info('start generating graphs of events from sensor 1')
     graphs_sensor_1 = []
     for event in sensor1_events:
         graphs_sensor_1 += gen_graphs(event, output_records, 'rh_in_specific_g_kg', 'linear1_sh')
+        graphs_sensor_1 += gen_graphs(event, output_records, 'rh_in_absolute_g_m3', 'linear1_ah')
 
     graphs.gen(graphs_sensor_1, 'sensor1_' + output_filename, 0, 0, global_range=True)
     logging.info('end generating graphs of events from sensor 1')
@@ -185,10 +200,12 @@ def main(events_file: str, start_shift: int, end_shift: int, output_filename: st
 
     sensor2_events = FilterUtil.measured_values_not_empty(sensor2_events, 'rh_in2_specific_g_kg')
     sensor2_events = FilterUtil.measured_values_not_empty(sensor2_events, 'rh_in2_absolute_g_m3')
+    sensor2_events = FilterUtil.measured_values_not_empty(sensor2_events, 'temperature_in2_celsius')
     logging.info('events after applying the filter: %d' % len(sensor2_events))
 
     linear_reg(sensor2_events, 'rh_in2_specific_g_kg', 'linear2_sh')
     linear_reg(sensor2_events, 'rh_in2_absolute_g_m3', 'linear2_ah')
+    linear_reg(sensor2_events, 'temperature_in2_celsius', 'linear2_temp')
 
     humidity_info_csv(sensor2_events, start_shift, end_shift)
 
