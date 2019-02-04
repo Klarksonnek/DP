2dd30a228bd82b2d65660310da970f4cf549f2b4
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-02-04 12:05:45 +0100

    dm.Attributes: new class SecondDifferenceAttr
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -200,3 +200,45 @@ class SecondDifferenceAttr(FirstDifferenceAttrB):
             last_value = value
 
         return before_second, after_second
+
+
+class GrowthRate(AbstractPrepareAttr):
+    def execute(self, timestamp, column, precision, intervals_before, intervals_after,
+                value_delay):
+        """Vypocet tempa rastu.
+
+        Vypocet tempa rastu sa vypocita ako pomer y_t/y_t_-1. Hodnota y_t sa vyberie na
+        zaklade posunu dopredu/dozadu a hodnota y_t_-1 sa vyberie ako posun o value_delay
+        do historie od hodnoty y_t.
+
+        :param timestamp: stred okienka, ktory sa pouzije ako bod od ktoreho sa posuva
+        :param column: stlpec, pre ktory sa maju spocitat atributy
+        :param precision: presnost vysledku
+        :param intervals_before: intervaly pred udalostou
+        :param intervals_after: interaly po udalosti
+        :param value_delay: posun do historie, z ktorej sa vyberie hodnota pre vypocet
+        :return:
+        """
+
+        before = []
+        after = []
+
+        for interval in intervals_before:
+            value_time = timestamp - interval
+            y_t = self.select_one_row(column, value_time)
+            y_t_1 = self.select_one_row(column, value_time - value_delay)  # t-1
+
+            ratio = round(y_t / y_t_1, precision)
+            name = self.attr_name(column, 'before', interval)
+            before.append((name, ratio))
+
+        for interval in intervals_after:
+            value_time = timestamp + interval
+            y_t = self.select_one_row(column, value_time)
+            y_t_1 = self.select_one_row(column, value_time - value_delay)  # t-1
+
+            ratio = round(y_t / y_t_1, precision)
+            name = self.attr_name(column, 'after', interval)
+            after.append((name, ratio))
+
+        return before, after
