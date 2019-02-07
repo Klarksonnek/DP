051662553e8a90a4a6d20bee5c1008d3c059b6ac
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-02-07 12:49:56 +0100

    dm.Attribute: new class LinearSimpleCachedRowSelector
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -11,6 +11,7 @@ sys.path.append(CODE_DIR)
 
 from dm.DateTimeUtil import DateTimeUtil
 from dm.Storage import Storage
+from scipy import stats
 
 
 class AttributeUtil:
@@ -204,6 +205,51 @@ class SimpleCachedRowSelector(AbstractRowSelector):
             if res is not None and res[0] is not None:
                 self.cache[column_name][time] = float(res[0])
                 value = float(res[0])
+
+        if value is None:
+            t = DateTimeUtil.utc_timestamp_to_str(time, '%Y/%m/%d %H:%M:%S')
+            raise ValueError('empty value at %s' % t)
+
+        return value
+
+
+class LinearSimpleCachedRowSelector(AbstractRowSelector):
+    def __init__(self, con, table_name, half_window_size):
+        self.cache = {}
+        self.half_window_size = half_window_size
+        super(LinearSimpleCachedRowSelector, self).__init__(con, table_name)
+
+    def row(self, column_name, time):
+        if column_name not in self.cache:
+            self.cache[column_name] = {}
+
+        if time in self.cache[column_name]:
+            value = self.cache[column_name][time]
+        else:
+            start = time - self.half_window_size
+            end = time + self.half_window_size
+            res = Storage.select_interval(self.con, start, end, column_name, self.table_name,
+                                          without_none_value=False)
+
+            error = False
+            if res is None or None in res:
+                error = True
+
+            if error:
+                self.cache[column_name][time] = None
+                t = DateTimeUtil.utc_timestamp_to_str(time, '%Y/%m/%d %H:%M:%S')
+                raise ValueError('empty value at %s' % t)
+
+            x = []
+            y = []
+            for i in range(0, len(res)):
+                x.append(i)
+                y.append(res[i])
+
+            slope, intercept, _, _, _ = stats.linregress(x, y)
+
+            value = intercept + slope * self.half_window_size
+            self.cache[column_name][time] = value
 
         if value is None:
             t = DateTimeUtil.utc_timestamp_to_str(time, '%Y/%m/%d %H:%M:%S')
