8bdf5a381f614e749ecb7033790f1b748a4d8e33
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-02-07 08:38:03 +0100

    dm.Attribute: new class AbstractRowSelector
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -161,6 +161,17 @@ class AttributeUtil:
         return attrs
 
 
+class AbstractRowSelector(ABC):
+    def __init__(self, con, table_name):
+        self.con = con
+        self.table_name = table_name
+        super(AbstractRowSelector, self).__init__()
+
+    @abstractmethod
+    def row(self, column_name, time):
+        pass
+
+
 # https://www.smartfile.com/blog/abstract-classes-in-python/
 # https://code.tutsplus.com/articles/understanding-args-and-kwargs-in-python--cms-29494
 # http://homel.vsb.cz/~dor028/Casove_rady.pdf
