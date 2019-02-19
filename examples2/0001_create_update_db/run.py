db16c23600122ec6b10fa1786744e2569b1d41ac
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-02-19 13:27:09 +0100

    0001_create_update_db: new system for generating databases
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- examples2/0001_create_update_db/run.py
+++ examples2/0001_create_update_db/run.py
@@ -25,6 +25,11 @@ def create_update_table(con, clients, start, end, devices, write_each, table_nam
     step_size = 600
     time_shift = 1200
     min_commit_size = 5000
+
+    # odstranenie posledneho intervalu, ktory mohol obsahovat chybajuce hodnoty
+    # z dovodu, ze tento interval este neexistoval
+    DBUtil.delete_from_time(con, table_name, step_size)
+
     last_inserted_row = DBUtil.last_inserted_values(con, table_name)
 
     last_open_close_state = 0
@@ -67,8 +72,60 @@ def create_update_table(con, clients, start, end, devices, write_each, table_nam
     logging.info('table %s created and updated' % table_name)
 
 
+def peto_intrak_db(con, cls, start, end, devs):
+    # v tomto case doslo k zmene DeviceID Protronix CO2 senzora
+    middle = int(DateTimeUtil.local_time_str_to_utc('2019/02/20 03:00:00').timestamp())
+
+    # full db
+    table_pt = 'measured_peto'
+    DBUtil.create_table(con, table_pt)
+    create_update_table(con, cls, start, middle, devs['peto'], 1, table_pt)
+    create_update_table(con, cls, middle, end, devs['peto2'], 1, table_pt)
+
+    # faster db
+    table_pt = 'measured_peto_reduced'
+    DBUtil.create_table(con, table_pt)
+    create_update_table(con, cls, start, middle, devs['peto'], 15, table_pt)
+    create_update_table(con, cls, middle, end, devs['peto2'], 15, table_pt)
+
+
+def klarka_izba_db(con, cls, start, end, devs):
+    table_kl = 'measured_klarka'
+    DBUtil.create_table(con, table_kl)
+    create_update_table(con, cls, start, end, devs['klarka'], 1, table_kl)
+
+    table_kl = 'measured_klarka_reduced'
+    DBUtil.create_table(con, table_kl)
+    create_update_table(con, cls, start, end, devs['klarka'], 15, table_kl)
+
+    # druha DB obsahuje od urciteho datumu vonkajsi IQ Home senzor
+    middle = int(DateTimeUtil.local_time_str_to_utc('2019/02/19 12:00:00').timestamp())
+    table_kl2 = 'measured_klarka_iqhome'
+    DBUtil.create_table(con, table_kl2)
+    create_update_table(con, cls, start, middle, devs['klarka'], 1, table_kl2)
+    create_update_table(con, cls, middle, end, devs['klarka2'], 1, table_kl2)
+
+    table_kl2 = 'measured_klarka_iqhome_reduced'
+    DBUtil.create_table(con, table_kl2)
+    create_update_table(con, cls, start, middle, devs['klarka'], 15, table_kl2)
+    create_update_table(con, cls, middle, end, devs['klarka2'], 15, table_kl2)
+
+
+def klarka_sprcha_db(con, cls, start, end, devs):
+    # full db
+    table_kl = 'measured_klarka_shower'
+    DBUtil.create_table(con, table_kl)
+    create_update_table(con, cls, start, end, devs['klarka_shower1'], 1, table_kl)
+
+    # faster db
+    table_kl = 'measured_klarka_shower_reduced'
+    DBUtil.create_table(con, table_kl)
+    create_update_table(con, cls, start, end, devs['klarka_shower2'], 15, table_kl)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+    devs = devices()
 
     con = ConnectionUtil.create_con()
     cur = con.cursor()
@@ -82,42 +139,11 @@ if __name__ == '__main__':
     cls['rehivetech'].api_key = ConnectionUtil.api_key('rehivetech')
 
     # from 2018/09/20 00:01:00
-    start = int(DateTimeUtil.local_time_str_to_utc('2018/09/20 00:01:00').timestamp())
+    start = int(DateTimeUtil.local_time_str_to_utc('2018/09/20 01:00:00').timestamp())
     end = int(time.time())
 
-    # full db
-    table_pt = 'measured_peto'
-    DBUtil.create_table(con, table_pt)
-    # DBUtil.check_timestamp_order(con, table_pt)
-    create_update_table(con, cls, start, end, devices()['peto'], 1, table_pt)
+    peto_intrak_db(con, cls, start, end, devs)
+    klarka_izba_db(con, cls, start, end, devs)
 
-    table_kl = 'measured_klarka'
-    DBUtil.create_table(con, table_kl)
-    # DBUtil.check_timestamp_order(con, table_kl)
-    create_update_table(con, cls, start, end, devices()['klarka'], 1, table_kl)
-
-    # faster db
-    table_pt = 'measured_peto_reduced'
-    DBUtil.create_table(con, table_pt)
-    create_update_table(con, cls, start, end, devices()['peto'], 15, table_pt)
-
-    table_kl = 'measured_klarka_reduced'
-    DBUtil.create_table(con, table_kl)
-    create_update_table(con, cls, start, end, devices()['klarka'], 15, table_kl)
-
-    #
-    # shower
-    #
-    start = int(DateTimeUtil.local_time_str_to_utc('2018/07/18 00:06:00').timestamp())
-    end = int(time.time())
-
-    # full db
-    table_kl = 'measured_klarka_shower'
-    DBUtil.create_table(con, table_kl)
-    # DBUtil.check_timestamp_order(con, table_pt)
-    create_update_table(con, cls, start, end, devices()['klarka_shower2'], 1, table_kl)
-
-    # faster db
-    table_kl = 'measured_klarka_shower_reduced'
-    DBUtil.create_table(con, table_kl)
-    create_update_table(con, cls, start, end, devices()['klarka_shower2'], 15, table_kl)
+    start = int(DateTimeUtil.local_time_str_to_utc('2018/07/18 06:00:00').timestamp())
+    klarka_sprcha_db(con, cls, start, end, devs)
