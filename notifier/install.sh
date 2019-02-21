803e1b1ccf31fe65782b0a1e309bdb7b26a1b3e5
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-02-21 11:58:40 +0100

    co2-notifier: icon for application
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- notifier/install.sh
+++ notifier/install.sh
@@ -8,6 +8,7 @@ cp -r ../dm/*.py /opt/notifier/dm/.
 
 mkdir /opt/notifier/notifier
 cp co2-notifier.py /opt/notifier/notifier/co2_notifier.py
+cp icon.png /opt/notifier/icon.png
 
 cp co2-notifier.desktop /usr/share/applications/.
 update-desktop-database
