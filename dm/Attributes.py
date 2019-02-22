63a488bd3e3a2054389f736a2b9c48bc7600a17e
Klara Necasova <xnecas24@stud.fit.vutbr.cz>
2019-02-22 07:41:37 +0100

    Attributes: DiffInLinear: new class
    Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -844,3 +844,17 @@ class VentilationLength(AbstractPrepareAttr):
         after = []
 
         return before, after
+
+
+class DiffInLinear(InLinear):
+    def execute(self, timestamp_before, timestamp_after, column, precision,
+                start_before, end_before, start_after, end_after, prefix):
+        b, a = super(DiffInLinear, self).execute(timestamp_before, timestamp_after,
+                                                 column, precision,
+                                                 start_before, end_before,
+                                                 start_after, end_after, prefix)
+
+        name = self.attr_name(column, prefix, 'before', '')
+        before = [(name, round(b[0][1] - a[0][1], 2))]
+
+        return before, []
