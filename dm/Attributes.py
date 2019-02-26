4f67d083ef5aebf49861d2735a6da5c296d6c2d3
Klara Necasova <xnecas24@stud.fit.vutbr.cz>
2019-02-26 10:12:09 +0100

    Attributes: support for SH in and SH out diff of sensor 1
    Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -238,6 +238,11 @@ class SimpleDiffRowSelector(SimpleCachedRowSelector):
             v2 = super(SimpleDiffRowSelector, self).row('temperature_out_celsius', time)
             value = v1 - v2
 
+        elif column_name == 'rh_in_specific_g_kg_diff_in_out':
+            v1 = super(SimpleDiffRowSelector, self).row('rh_in_specific_g_kg', time)
+            v2 = super(SimpleDiffRowSelector, self).row('rh_out_specific_g_kg', time)
+            value = v1 - v2
+
         elif column_name == 'rh_in2_percentage_diff':
             v1 = super(SimpleDiffRowSelector, self).row('rh_in2_percentage', time)
             v2 = super(SimpleDiffRowSelector, self).row('rh_out_percentage', time)
