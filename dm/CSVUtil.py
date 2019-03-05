de604c33843a2a68cd15cc7f50b1c7df43f7102a
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-03-05 22:52:21 +0100

    dm.CSVUtil: create_csv_file() log if data are empty
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/CSVUtil.py
+++ dm/CSVUtil.py
@@ -1,9 +1,14 @@
 import csv
+import logging
 
 
 class CSVUtil:
     @staticmethod
     def create_csv_file(data: list, filename: str):
+        if data == []:
+            logging.warning('empty data to write')
+            return
+
         field_names = []
         for key, _ in data[0].items():
             field_names.append(key)
