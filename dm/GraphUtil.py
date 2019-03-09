05742c8bf2c892e103a6094b0c24435a916a4cb6
Klara Necasova <xnecas24@stud.fit.vutbr.cz>
2019-03-09 11:54:46 +0100

    dm.GraphUtil: new function for stacked bar graph
    Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

--- dm/GraphUtil.py
+++ dm/GraphUtil.py
@@ -66,3 +66,28 @@ class GraphUtil:
 
         if 'show' in action:
             plt.show()
+
+    @staticmethod
+    def gen_stacked_bar_graph(first_col, second_col, third_col):
+        ind = np.arange(3)
+        width = 0.35
+        y_offset = np.zeros(3)
+        fig = plt.figure()
+
+        p1 = plt.bar(ind, first_col, width, bottom=y_offset)
+        y_offset = y_offset + first_col
+
+        p2 = plt.bar(ind, second_col, width, bottom=y_offset)
+        y_offset = y_offset + second_col
+
+        p3 = plt.bar(ind, third_col, width, bottom=y_offset)
+        plt.xticks(ind, ('5 minutes', '10 minutes', '25 minutes'))
+
+        plt.legend((p1[0], p2[1], p3[2]), ('pred 5 min', 'pred 10 min', 'pred 25 min'),
+                   loc='lower center', bbox_to_anchor=(0.5, -0.1), bbox_transform=fig.transFigure)
+
+        plt.subplots_adjust(bottom=0.15)
+
+        fig.savefig('result.png', bbox_inches="tight")
+
+        plt.show()
