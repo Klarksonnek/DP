898020607fab580cf415d38c9fb30f8f72603187
Klara Necasova <xnecas24@stud.fit.vutbr.cz>
2019-03-11 15:11:58 +0100

    dm.Attributes: new class AbstractLineCoefficients
    Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -975,21 +975,21 @@ class DiffInLinear(InLinear):
         return before, []
 
 
-class DistanceToLine:
-    def __init__(self, training):
-        self.training = training
-        self.model = None
+class AbstractLineCoefficients(ABC):
+    def __init__(self):
+        super(AbstractLineCoefficients, self).__init__()
 
-    def ventilation_length_events(self, training: list, ventilation_length: int):
-        out = []
+    @abstractmethod
+    def calculate(self, data, interval, col1, col2, col3, point_x, point_y):
+        pass
 
-        for row in training:
-            if row['VentilationLength_event__'] == "'" + str(ventilation_length) + "'":
-                out.append(row)
+    def convert_line(self, coeffs):
+        if coeffs[1] == 0:
+            return self._convert_line_to_general_without_c(coeffs)
 
-        return out
+        return self._convert_line_to_general(coeffs)
 
-    def convert_line_to_general(self, coeffs):
+    def _convert_line_to_general(self, coeffs):
         """ Converts line equation y = kx + q to the form ax + by + c = 0 (general form)
         """
 
@@ -1021,6 +1021,43 @@ class DistanceToLine:
         b = L
 
         return a, b, c
+
+    def _convert_line_to_general_without_c(self, coeffs):
+        """ Converts line equation y = kx to the form ax + by = 0 (general form)
+        """
+
+        # represents coeffs as fractions
+        tmp = Fraction(str(coeffs[0])).limit_denominator(1000)
+        n1 = tmp.numerator
+        d1 = tmp.denominator
+
+        # symbolic variables
+        x = var('x')
+
+        y1 = (n1 * x) / d1
+
+        y_mult1 = y1 * d1
+
+        a = y_mult1.subs('x', 1) * (-1)
+
+        b = d1
+
+        return a, b, 0
+
+
+class DistanceToLine:
+    def __init__(self, training):
+        self.training = training
+        self.model = None
+
+    def ventilation_length_events(self, training: list, ventilation_length: int):
+        out = []
+
+        for row in training:
+            if row['VentilationLength_event__'] == "'" + str(ventilation_length) + "'":
+                out.append(row)
+
+        return out
 
     def humidity_clusters(self, training, col1, col2, col3, intervals):
         # colors
