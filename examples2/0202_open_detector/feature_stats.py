b35126f96310291d65dba624f08e2a380c45e050
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-03-20 19:25:05 +0100

    feature_stats: refactoring
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- examples2/0202_open_detector/feature_stats.py
+++ examples2/0202_open_detector/feature_stats.py
@@ -75,6 +75,28 @@ OUTPUT_FILENAME = 'out.csv'
 BEFORE_TIME = 10 * 60
 AFTER_TIME = 10 * 60
 
+
+def generate_row(str_process, performance, records, duration1, duration2):
+    total = performance['nothing_as_true_nothing']
+    total += performance['open_as_true_nothing']
+    total += performance['nothing_as_true_open']
+    total += performance['open_as_true_open']
+
+    output = str_process
+    output += '{0:6.2f}%   '.format(performance['accuracy'])
+    output += '{0:7}    '.format(records)
+    output += '{0:7} '.format(total)
+    output += '{0:12}   '.format(performance['nothing_as_true_nothing'])
+    output += '{0:13}   '.format(performance['open_as_true_nothing'])
+    output += '{0:14}   '.format(performance['nothing_as_true_open'])
+    output += '{0:13}'.format(performance['open_as_true_open'])
+
+    if duration1 != '' and duration2 != '':
+        output += '      {0}  {1}'.format(str(duration1)[:9], str(duration2)[:9])
+
+    return output
+
+
 from subprocess import PIPE, run
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
@@ -84,19 +106,15 @@ if __name__ == '__main__':
     out = {}
     header = '{0:40} '.format('               filter')
     header += '{0:8}   '.format('accuracy')
-    header += '{0:12}   '.format('not_true_not')
-    header += '{0:13}   '.format('open_true_not')
-    header += '{0:14}   '.format('open_true_open')
-    header += '{0:13}   '.format('not_true_open')
-
-    header += '{0:8}   '.format('accuracy')
+    header += '{0:7}   '.format('records')
+    header += '  {0:3}      '.format('sum')
     header += '{0:12}   '.format('not_true_not')
     header += '{0:13}   '.format('open_true_not')
     header += '{0:14}   '.format('open_true_open')
     header += '{0:13}   '.format('not_true_open')
 
     header += '{0:9}  '.format('duration')
-    header += '{0:9}  '.format('duration')
+    header += '{0:9}'.format('duration')
 
     logging.debug(header)
 
@@ -112,9 +130,9 @@ if __name__ == '__main__':
         start_time = time.monotonic()
         result = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         end_time = time.monotonic()
-        duration = timedelta(seconds=end_time - start_time)
+        duration1 = timedelta(seconds=end_time - start_time)
 
-        output = '{0:40} '.format(process)
+        str_process = '{0:40} '.format(process)
 
         if os.path.isfile('out.csv'):
             start_time2 = time.monotonic()
@@ -122,19 +140,10 @@ if __name__ == '__main__':
             _, _, p1 = p.simple()
             table2, wrong2, p2 = p.with_delay(BEFORE_TIME, AFTER_TIME)
             end_time2 = time.monotonic()
-            duration2 = timedelta(seconds=end_time2 - start_time2)
 
-            output += '{0:6.2f}%'.format(p1['accuracy'])
-            output += '{0:12}   '.format(p1['nothing_as_true_nothing'])
-            output += '{0:13}   '.format(p1['open_as_true_nothing'])
-            output += '{0:14}   '.format(p1['open_as_true_open'])
-            output += '{0:13}   '.format(p1['nothing_as_true_open'])
-            output += '    {0:6.2f}%'.format(p2['accuracy'])
-            output += '{0:12}   '.format(p2['nothing_as_true_nothing'])
-            output += '{0:13}   '.format(p2['open_as_true_nothing'])
-            output += '{0:14}   '.format(p2['open_as_true_open'])
-            output += '{0:13}   '.format(p2['nothing_as_true_open'])
-            output += '    {0}  {1}'.format(str(duration)[:9], str(duration2)[:9])
-            logging.debug(output)
+            duration2 = timedelta(seconds=end_time2 - start_time2)
+            logging.debug(generate_row(str_process, p1, p.count, duration1, duration2))
+            logging.debug(generate_row(str_process, p2, p.count, '', ''))
+            logging.debug('')
         else:
-            logging.error('{0:190} {1}  0:00:00.0'.format(process, str(duration)[:9]))
+            logging.error('{0:190} {1}  0:00:00.0'.format(process, str(duration1)[:9]))
