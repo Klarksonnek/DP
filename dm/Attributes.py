0de2f190719a786ab1a995b28b47bf3b1fece5de
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-04-05 22:47:09 +0200

    dm.Attributes: new class ExpRegressionWithDelay
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -1071,6 +1071,35 @@ class SimpleExpRegression(AbstractRegression):
         return out
 
 
+class ExpRegressionWithDelay(SimpleExpRegression):
+    def __init__(self, co2_out, volume, window_size, threshold):
+        self._window_size = window_size
+        self._threshold = threshold
+        super(ExpRegressionWithDelay, self).__init__(co2_out, volume)
+
+    def compute_parameter(self, x, y):
+        delay = ValueUtil.detect_sensor_delay(x, self._window_size, self._threshold)
+        return super(ExpRegressionWithDelay, self).compute_parameter(x[delay:], y[delay:])
+
+    def compute_curve(self, x, y):
+        delay = ValueUtil.detect_sensor_delay(y, self._window_size, self._threshold)
+
+        new_x = []
+        for i in range(0, len(x) - delay):
+            new_x.append(i)
+
+        values = super(ExpRegressionWithDelay, self).compute_curve(new_x, y[delay:])
+
+        if delay == 0:
+            return values
+
+        out = []
+        for k in range(0, delay):
+            out.append(y[k])
+
+        return out + values
+
+
 class AbstractLineCoefficients(ABC):
     def __init__(self):
         super(AbstractLineCoefficients, self).__init__()
