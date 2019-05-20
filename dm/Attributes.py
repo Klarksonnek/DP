99101203bd04c2aca9877a5c8a9f992ac721fdf6
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-05-20 23:56:22 +0200

    dm.CenterLineCoefficients: extract class
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -312,12 +312,7 @@ class AttributeUtil:
         return out
 
 
-class CenterLineCoefficients(AbstractLineCoefficients):
-    def calculate(self, data, interval, col1, col2, col3, point_x, point_y):
-        a = point_y
-        b = -point_x
 
-        return -a / b
 
 
 class DistanceToLine:
---
+++ dm/CenterLineCoefficients.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+
+from os.path import dirname, abspath, join
+import os
+from functools import reduce
+import sys
+import logging
+import math
+import csv
+
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '../..', ''))
+sys.path.append(CODE_DIR)
+
+from dm.DateTimeUtil import DateTimeUtil
+from dm.CSVUtil import CSVUtil
+from dm.Storage import Storage
+from dm.ValueUtil import ValueUtil
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from fractions import Fraction
+from sympy import *
+from scipy.optimize import curve_fit
+from scipy.spatial import ConvexHull
+
+DATA_CACHE = None
+
+
+from dm.AbstractLineCoefficients import AbstractLineCoefficients
+
+
+class CenterLineCoefficients(AbstractLineCoefficients):
+    def calculate(self, data, interval, col1, col2, col3, point_x, point_y):
+        a = point_y
+        b = -point_x
+
+        return -a / b
+
