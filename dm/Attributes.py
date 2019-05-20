9752c075e4425993e5a962292a6119a7537ccb63
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-05-20 23:29:04 +0200

    dm.SimpleRowSelector: extract class
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -312,20 +312,6 @@ class AttributeUtil:
         return out
 
 
-class SimpleRowSelector(AbstractRowSelector):
-    def row(self, column_name, time):
-        res = Storage.one_row(self.con, self.table_name, column_name, time)
-
-        if res is None or res[0] is None:
-            t = DateTimeUtil.utc_timestamp_to_str(time, '%Y/%m/%d %H:%M:%S')
-            raise ValueError('empty value at %s' % t)
-
-        return float(res[0])
-
-    def clear(self):
-        pass
-
-
 class SimpleCachedRowSelector(AbstractRowSelector):
     def __init__(self, con, table_name):
         self.cache = {}
