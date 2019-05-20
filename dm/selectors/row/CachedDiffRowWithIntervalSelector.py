c58c8ebf96ec06bcf4673c1ddbade642df1e3608
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-05-21 00:26:35 +0200

    dm.selectors.row: new package
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/selectors/row/CachedDiffRowWithIntervalSelector.py
+++ dm/selectors/row/CachedDiffRowWithIntervalSelector.py
@@ -1,34 +1,13 @@
-from abc import ABC, abstractmethod
-from collections import OrderedDict
-
 from os.path import dirname, abspath, join
-import os
-from functools import reduce
 import sys
-import logging
-import math
-import csv
 
 THIS_DIR = dirname(__file__)
 CODE_DIR = abspath(join(THIS_DIR, '../..', ''))
 sys.path.append(CODE_DIR)
 
-from dm.DateTimeUtil import DateTimeUtil
-from dm.CSVUtil import CSVUtil
-from dm.Storage import Storage
-from dm.ValueUtil import ValueUtil
-from scipy import stats
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from fractions import Fraction
-from sympy import *
-from scipy.optimize import curve_fit
-from scipy.spatial import ConvexHull
-
 DATA_CACHE = None
 
-from dm.CachedRowWithIntervalSelector import CachedRowWithIntervalSelector
+from dm.selectors.interval.CachedRowWithIntervalSelector import CachedRowWithIntervalSelector
 
 class CachedDiffRowWithIntervalSelector(CachedRowWithIntervalSelector):
     def row(self, column_name, time):
