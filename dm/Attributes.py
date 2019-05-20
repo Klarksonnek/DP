9443b34bcebf5f6e183bec6d9e9e9bc565a43347
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2019-05-20 23:29:35 +0200

    dm.SimpleCachedRowSelector: extract class
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- dm/Attributes.py
+++ dm/Attributes.py
@@ -312,35 +312,6 @@ class AttributeUtil:
         return out
 
 
-class SimpleCachedRowSelector(AbstractRowSelector):
-    def __init__(self, con, table_name):
-        self.cache = {}
-        super(SimpleCachedRowSelector, self).__init__(con, table_name)
-
-    def row(self, column_name, time):
-        if column_name not in self.cache:
-            self.cache[column_name] = {}
-
-        value = None
-        if time in self.cache[column_name]:
-            value = self.cache[column_name][time]
-        else:
-            res = Storage.one_row(self.con, self.table_name, column_name, time)
-
-            if res is not None and res[0] is not None:
-                self.cache[column_name][time] = float(res[0])
-                value = float(res[0])
-
-        if value is None:
-            t = DateTimeUtil.utc_timestamp_to_str(time, '%Y/%m/%d %H:%M:%S')
-            raise ValueError('empty value at %s' % t)
-
-        return value
-
-    def clear(self):
-        del self.cache
-
-
 class LinearSimpleCachedRowSelector(AbstractRowSelector):
     def __init__(self, con, table_name, half_window_size):
         self.cache = {}
---
+++ dm/SimpleCachedRowSelector.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+
+from os.path import dirname, abspath, join
+import os
+from functools import reduce
+import sys
+import logging
+import math
+import csv
+
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '../..', ''))
+sys.path.append(CODE_DIR)
+
+from dm.DateTimeUtil import DateTimeUtil
+from dm.CSVUtil import CSVUtil
+from dm.Storage import Storage
+from dm.ValueUtil import ValueUtil
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from fractions import Fraction
+from sympy import *
+from scipy.optimize import curve_fit
+from scipy.spatial import ConvexHull
+
+DATA_CACHE = None
+
+
+
+from dm.AbstractRowSelector import AbstractRowSelector
+class SimpleCachedRowSelector(AbstractRowSelector):
+    def __init__(self, con, table_name):
+        self.cache = {}
+        super(SimpleCachedRowSelector, self).__init__(con, table_name)
+
+    def row(self, column_name, time):
+        if column_name not in self.cache:
+            self.cache[column_name] = {}
+
+        value = None
+        if time in self.cache[column_name]:
+            value = self.cache[column_name][time]
+        else:
+            res = Storage.one_row(self.con, self.table_name, column_name, time)
+
+            if res is not None and res[0] is not None:
+                self.cache[column_name][time] = float(res[0])
+                value = float(res[0])
+
+        if value is None:
+            t = DateTimeUtil.utc_timestamp_to_str(time, '%Y/%m/%d %H:%M:%S')
+            raise ValueError('empty value at %s' % t)
+
+        return value
+
+    def clear(self):
+        del self.cache
